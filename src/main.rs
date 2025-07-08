a0bfdab55b9faa38ce2997d298e2f429469203b9
John Graham <johngavingraham@googlemail.com>
2025-07-08 17:27:39 +0100

    Remove old code

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,7 @@ use rayon::iter::{IntoParallelIterator, ParallelIterator};
 
 use vm_parser::vm_execute;
 
-#[derive(Debug)]
-struct Program {
-    instrs: Vec<Instruction>,
-}
+struct Program {}
 
 impl Program {
     fn execute(&self, vx: f64, vy: f64) -> f64 {
@@ -14,81 +11,8 @@ impl Program {
     }
 }
 
-impl From<&str> for Program {
-    fn from(s: &str) -> Self {
-        Self {
-            instrs: s.lines().skip(1).map(|line| line.into()).collect(),
-        }
-    }
-}
-
-#[derive(Debug)]
-struct Instruction {
-    out: usize,
-    op: Op,
-}
-
-fn parse_id(id: &str) -> usize {
-    usize::from_str_radix(&id[1..], 16).unwrap()
-}
-
-impl From<&str> for Instruction {
-    fn from(s: &str) -> Self {
-        let mut words = s.split_ascii_whitespace();
-        let out = parse_id(words.next().unwrap());
-        let op = match words.next().unwrap() {
-            "var-x" => Op::VarX,
-            "var-y" => Op::VarY,
-            "add" => Op::Add(
-                parse_id(words.next().unwrap()),
-                parse_id(words.next().unwrap()),
-            ),
-            "sub" => Op::Sub(
-                parse_id(words.next().unwrap()),
-                parse_id(words.next().unwrap()),
-            ),
-            "mul" => Op::Mul(
-                parse_id(words.next().unwrap()),
-                parse_id(words.next().unwrap()),
-            ),
-            "max" => Op::Max(
-                parse_id(words.next().unwrap()),
-                parse_id(words.next().unwrap()),
-            ),
-            "min" => Op::Min(
-                parse_id(words.next().unwrap()),
-                parse_id(words.next().unwrap()),
-            ),
-            "neg" => Op::Neg(parse_id(words.next().unwrap())),
-            "sqrt" => Op::Sqrt(parse_id(words.next().unwrap())),
-            "square" => Op::Square(parse_id(words.next().unwrap())),
-            "const" => Op::Const(words.next().unwrap().parse().unwrap()),
-            operation => {
-                panic!("Unrecognised operation: {operation:?}");
-            }
-        };
-
-        Self { out, op }
-    }
-}
-
-#[derive(Debug)]
-enum Op {
-    VarX,
-    VarY,
-    Add(usize, usize),
-    Sub(usize, usize),
-    Mul(usize, usize),
-    Max(usize, usize),
-    Min(usize, usize),
-    Neg(usize),
-    Sqrt(usize),
-    Square(usize),
-    Const(f64),
-}
-
 fn main() {
-    let program: Program = include_str!("../prospero.vm").into();
+    let program: Program = Program {};
     let mut img = GrayImage::new(1024, 1024);
     let img_usize = &mut img as *const _ as usize;
     (0..1024).into_par_iter().for_each(|y| {
